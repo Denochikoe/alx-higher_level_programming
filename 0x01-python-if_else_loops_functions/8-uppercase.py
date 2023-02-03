efc9f345bc921bdddc59bd17dc4f5d17f837ff11
root <Denochikoe>
2023-02-03 09:20:15 +0000

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+	for a in str:
+		if ord('a') <= ord(a) <= ord('z'):
+			a = chr(ord(a) - (ord('a') - ord('A')))
+		print("{:s}".format(a), end='')
+	print("")
+	
+ 
