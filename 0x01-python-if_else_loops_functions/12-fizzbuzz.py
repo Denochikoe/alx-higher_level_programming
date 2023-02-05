6173e368235371b9f215d1f3ac8abeb3dcdaa602
root <Denochikoe>
2023-02-05 18:42:41 +0000

    100-print_tebahpla.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+	for a in range(1, 101):
+		string = ""
+
+		if num % 3 == 0:
+			string = string + "Fizz"
+		if num % 5 == 0:
+			string = string + "Buzz"
+		if num % 3 == 0 and num % 5 ==0:
+			string = string + "FizzBuzz"
+		else:
+			string = string + str(num)
+	print(string)
