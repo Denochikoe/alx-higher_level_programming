273d38def27b6b4fed3f1d54c8ace06945e942c0
Denochikoe <denochikoe@gmail.com>
2024-08-23 20:07:39 +0300

    fix

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 import this
-zen_of_python = this.s
-rot13_start_marker = 'Gur Mra bs Clguba, ol Gvz Crgref'
-index = zen_of_python.find(rot13_start_marker)
-if index != -1:
-    english_part = zen_of_python[:index].strip()
-else:
-    english_part = zen_of_python.strip()
-print(english_part, end='')
+print(this.s.split('Gur Mra bs Clguba')[0].strip(), end='')
