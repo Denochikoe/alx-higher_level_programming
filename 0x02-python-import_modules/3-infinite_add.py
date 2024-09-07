7edc6a7fe7d6099bcd151e73dfbac5d36ec00d19
Denochikoe <denochikoe@gmail.com>
2024-09-07 16:42:13 +0300

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 import sys
 
+
 def main():
-    # Get all arguments except the script name itself
     args = sys.argv[1:]
     
     # Convert arguments to integers and sum them up
-    total = sum(int(arg) for arg in args)
-    
+    total = sum(int(arg) for arg in args) 
     # Print the result
     print(total)
+
 
 # Ensure the code only runs when executed directly
 if __name__ == "__main__":
