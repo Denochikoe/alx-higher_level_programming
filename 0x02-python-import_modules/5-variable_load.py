3873cb3a6d336d0cfc8f6416c426c715f51758ae
Denochikoe <denochikoe@gmail.com>
2024-09-07 17:24:50 +0300

    5-variable_load.py

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from variable_load_5 import a
+
+
+def main():
+    # Print the value of the variable a
+    print(a)
+
+
+# Ensure the code only runs when executed directly
+if __name__ == "__main__":
+    main()
+
