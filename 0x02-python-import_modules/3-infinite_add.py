a0e35e530ebeaeae95d6b3974ad650a56e4da626
Denochikoe <denochikoe@gmail.com>
2024-09-07 16:44:59 +0300

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,8 +4,6 @@ import sys
 
 def main():
     args = sys.argv[1:]
-    
-    # Convert arguments to integers and sum them up
     total = sum(int(arg) for arg in args) 
     # Print the result
     print(total)
