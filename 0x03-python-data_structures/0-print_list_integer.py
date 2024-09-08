357c56e818cc57cdeee2d818a9ed98a37f7a4985
Denochikoe <denochikoe@gmail.com>
2024-09-08 17:42:18 +0300

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,4 +4,4 @@
 def print_list_integer(my_list=[]):
     for number in my_list:
         # Use str.format() to print each integer
-        print("{}".format(number))
+        print("{:d}".format(number))
