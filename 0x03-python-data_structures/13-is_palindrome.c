47c99994017dcffcd5fe82f8bc7c73047973fdec
Denochikoe <denochikoe@gmail.com>
2024-09-08 22:09:22 +0300

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,7 +4,7 @@
 
 /**
  * reverse_list - Reverses a linked list
- * @head: Pointer to pointer of the first node of the list
+ * @head: Pointer to the first node of the list
  * Return: Pointer to the new head of the reversed list
  */
 listint_t *reverse_list(listint_t *head)
@@ -26,7 +26,7 @@ listint_t *reverse_list(listint_t *head)
 
 /**
  * is_palindrome - Checks if a linked list is a palindrome
- * @head: Pointer to pointer of the first node of the list
+ * @head: Pointer to the first node of the list
  * Return: 1 if palindrome, 0 otherwise
  */
 int is_palindrome(listint_t **head)
@@ -35,22 +35,22 @@ int is_palindrome(listint_t **head)
     listint_t *copy_of_second_half;
     int palindrome = 1;
 
-    // If list is empty or has only one element
+    /* If list is empty or has only one element */
     if (*head == NULL || (*head)->next == NULL)
         return 1;
 
-    // Find the middle of the list
+    /* Find the middle of the list */
     while (fast != NULL && fast->next != NULL)
     {
         fast = fast->next->next;
         slow = slow->next;
     }
 
-    // Reverse the second half
+    /* Reverse the second half */
     second_half = reverse_list(slow);
-    copy_of_second_half = second_half; // Save the start of the reversed list
+    copy_of_second_half = second_half; /* Save the start of the reversed list */
 
-    // Compare the first half and the reversed second half
+    /* Compare the first half and the reversed second half */
     first_half = *head;
     while (second_half != NULL)
     {
@@ -63,9 +63,8 @@ int is_palindrome(listint_t **head)
         second_half = second_half->next;
     }
 
-    // Restore the original list (optional)
+    /* Restore the original list (optional) */
     reverse_list(copy_of_second_half);
 
     return palindrome;
 }
-
