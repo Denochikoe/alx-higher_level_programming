3803f3ecde2acd5c395b388ce60b18ed58d3151c
Denochikoe <denochikoe@gmail.com>
2024-09-08 20:40:50 +0300

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,7 +2,6 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Ensure both tuples have exactly 2 elements by extending with zeros if necessary
     tuple_a = (tuple_a + (0, 0))[:2]
     tuple_b = (tuple_b + (0, 0))[:2]
     # Add the corresponding elements of the two tuples
