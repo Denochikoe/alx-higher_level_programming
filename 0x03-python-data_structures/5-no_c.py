56570a058fcf74daea987c35a1fbcf92cb83c1ef
Denochikoe <denochikoe@gmail.com>
2024-09-08 19:06:28 +0300

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,12 +4,10 @@
 def no_c(my_string):
     # Initialize an empty string to store the result
     new_string = ""
-    
     # Iterate over each character in the input string
     for char in my_string:
         # Add characters to the new string only if they are not 'c' or 'C'
         if char != 'c' and char != 'C':
             new_string += char
-    
     # Return the modified string without 'c' or 'C'
     return new_string
