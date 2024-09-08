6c07a07b4c38966f2b27c639a9bfc60bf527c594
Denochikoe <denochikoe@gmail.com>
2024-09-08 17:49:15 +0300

    1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    # Check if the index is within the valid range
+    if idx < 0 or idx >= len(my_list):
+        return None
+    # Return the element at the specified index
+    return my_list[idx]
