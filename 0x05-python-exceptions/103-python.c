e870f282745df735fbca3b9cb273b4984c023893
Denochikoe <denochikoe@gmail.com>
2024-09-26 21:17:41 +0300

    103-python.c

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -3,54 +3,57 @@
 
 void print_python_list(PyObject *p)
 {
-    Py_ssize_t i, size;
+    Py_ssize_t size, i;
     PyObject *item;
 
     if (!PyList_Check(p)) {
-        printf("[ERROR] Invalid List Object\n");
+        fprintf(stderr, "[ERROR] Invalid List Object\n");
         return;
     }
 
     size = ((PyVarObject *)p)->ob_size;
+
     printf("[*] Python list info\n");
     printf("[*] Size of the Python List = %zd\n", size);
     printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
-
     for (i = 0; i < size; i++) {
         item = ((PyListObject *)p)->ob_item[i];
         printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
     }
 }
 
-
 void print_python_bytes(PyObject *p) {
+    Py_ssize_t size;
+    char *string;
+
     if (!PyBytes_Check(p)) {
-        printf("Invalid PyBytesObject\n");
+        fprintf(stderr, "[ERROR] Invalid Bytes Object\n");
         return;
     }
 
-    Py_ssize_t size = 0;
-    const char *bytes_str = PyBytes_AsString(p);
-    for (size = 0; bytes_str[size] != '\0'; size++);
+    size = ((PyVarObject *)p)->ob_size;
+    string = ((PyBytesObject *)p)->ob_sval;
 
     printf("[*] Python bytes info\n");
-    printf("[*] Size of the Python Bytes = %zd\n", size);
-
-    printf("[*] First %zd bytes: ", (size > 10) ? 10 : size);
-    for (Py_ssize_t i = 0; i < ((size > 10) ? 10 : size); i++) {
-        printf("%02x ", (unsigned char)bytes_str[i]);
-    }
+    printf("[*] Size: %zd\n", size);
+    printf("[*] Trying string: %s\n", string);
+    printf("[*] First %zd bytes: ", size < 10 ? size + 1 : 10);
+	for (Py_ssize_t i = 0; i < size && i < 10; i++)
+	{
+        printf("%02x ", (unsigned char)string[i]);
+	}
     printf("\n");
 }
 
-void print_python_float(PyObject *p)
-{
+void print_python_float(PyObject *p) {
+    double value;
+
     if (!PyFloat_Check(p)) {
-        printf("Invalid PyFloatObject\n");
+        fprintf(stderr, "[ERROR] Invalid Float Object\n");
         return;
     }
 
-    double value = PyFloat_AsDouble(p);
+    value = ((PyFloatObject *)p)->ob_fval;
     printf("[*] Python float info\n");
-    printf("[*] Value of the float = %.10f\n", value);
+    printf("[*] Value: %f\n", value);
 }
